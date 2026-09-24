6f38f0c4f612fc86fb98d53c27bbb5d83404d917
CoolerSonic91 <32468032+CoolerSonic91@users.noreply.github.com>
2026-09-24 16:33:22 +0200

    Crear usuario_tests.js

---
+++ ET1/app/usuario_tests.js
@@ -0,0 +1,15 @@
+// Definicion de tests: usuario_def_tests
+
+var usuario_def_tests = [
+  // --------- DNI --------------------
+
+	// ----------- DNI ADD ----------    
+    
+    ["usuario","dni","input", 1 "Validar formato dni en ADD", "ADD", "dni_format_KO", "Formato de DNI no valido"],
+        ["usuario","dni","input", 2 "Validar letra dni en ADD (modulo 23)", "ADD", "dni_letra_KO", "La letra del DNI no se corresponde con los numeros"],
+	["usuario","dni","input", 3 "Validar dni correcto en ADD", "ADD", true, "DNI correcto"],
+    
+    // ------------- DNI EDIT ---------------
+    ["usuario","dni","input", 4 "Validar formato dni en EDIT", "EDIT", "dni_format_KO", "Formato de DNI no valido"],
+    
+];
